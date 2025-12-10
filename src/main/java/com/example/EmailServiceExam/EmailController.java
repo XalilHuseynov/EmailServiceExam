36ae65719c2a19971fa3b475d3d4d4bf3f1a7aed
Xalil Huseynov <0708802077nar@gmail.com>
2025-12-11 02:32:43 +0400

    deyisiklikler edildi

--- src/main/java/com/example/EmailServiceExam/EmailController.java
+++ src/main/java/com/example/EmailServiceExam/EmailController.java
@@ -4,7 +4,6 @@ package com.example.EmailServiceExam;
 import com.example.EmailServiceExam.dto.EmailRequestDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
